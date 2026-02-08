e9f9150cdc1fbda2312c83f6c2caf623795d8455
andres-lou <lourios.andres@gmail.com>
2026-02-08 13:28:24 -0600

    final test with initial config

--- flores_plus_translation_task/src/update_report.py
+++ flores_plus_translation_task/src/update_report.py
@@ -2,6 +2,10 @@ import json
 from sheets_create import update_report_spreadsheet
 from util import authenticate
 
+with open("../data/config.json") as f:
+    if not json.loads(f.read())['start_translation']:
+        exit(0)
+
 creds = authenticate()
 with open("../data/state.json") as f:
     state = json.loads(f.read())
